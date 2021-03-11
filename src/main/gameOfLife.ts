15dd7eab1c978e59189362c55f98555d0bbaa27e
Jing Yu <jing.yu@cazoo.co.uk>
2021-03-11 10:01:25 +0000

    mob next [ci-skip]

---
+++ src/main/gameOfLife.ts
@@ -0,0 +1,11 @@
+export class GameOfLife {
+  private grid: Number[][];
+
+  constructor(grid: Number[][]) {
+    this.grid = grid;
+  }
+
+  play(x: number, y: number) {
+    return 0;
+  }
+}
---
+++ src/test/gameOfLife.test.ts
@@ -0,0 +1,30 @@
+import { GameOfLife } from "../main/gameOfLife";
+
+describe("GameOfLife", () => {
+  it("should die if live cell have 0 live neighbours", () => {
+    let game: GameOfLife = new GameOfLife([
+      [0, 0, 0],
+      [0, 1, 0],
+      [0, 0, 0],
+    ]);
+    expect(game.play(1, 1)).toBe(0);
+  });
+
+  it("should die if live cell have 1 live neighbours", () => {
+    let game: GameOfLife = new GameOfLife([
+      [1, 0, 0],
+      [0, 1, 0],
+      [0, 0, 0],
+    ]);
+    expect(game.play(1, 1)).toBe(0);
+  });
+
+  it("should alive if live cell have 2 live neighbours", () => {
+    let game: GameOfLife = new GameOfLife([
+      [1, 1, 0],
+      [0, 1, 0],
+      [0, 0, 0],
+    ]);
+    expect(game.play(1, 1)).toBe(1);
+  });
+});

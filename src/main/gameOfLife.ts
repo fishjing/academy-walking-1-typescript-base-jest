def121a66c3a9ac10ad2e4ba35a0881d3e0ab1f1
Luke Marsh <luke.marsh@cazoo.co.uk>
2021-03-11 10:14:28 +0000

    mob next [ci-skip]

--- src/main/gameOfLife.ts
+++ src/main/gameOfLife.ts
@@ -1,11 +1,20 @@
 export class GameOfLife {
-  private grid: Number[][];
+  private grid: number[][];
 
-  constructor(grid: Number[][]) {
+  constructor(grid: number[][]) {
     this.grid = grid;
   }
 
   play(x: number, y: number) {
-    return 0;
+    let total = 0;
+    for (let i = x - 1; i < x + 2; i++) {
+      for (let j = y - 1; j < y + 2; j++) {
+        total += this.grid[i][j];
+      }
+    }
+    if (total < 3) {
+      return 0;
+    }
+    return 1;
   }
 }
--- src/test/gameOfLife.test.ts
+++ src/test/gameOfLife.test.ts
@@ -27,4 +27,22 @@ describe("GameOfLife", () => {
     ]);
     expect(game.play(1, 1)).toBe(1);
   });
+
+  it("should alive if live cell have 3 live neighbours", () => {
+    let game: GameOfLife = new GameOfLife([
+      [1, 1, 1],
+      [0, 1, 0],
+      [0, 0, 0],
+    ]);
+    expect(game.play(1, 1)).toBe(1);
+  });
+
+  it("should alive if live cell have 4 live neighbours", () => {
+    let game: GameOfLife = new GameOfLife([
+      [1, 1, 1],
+      [1, 1, 0],
+      [0, 0, 0],
+    ]);
+    expect(game.play(1, 1)).toBe(0);
+  });
 });

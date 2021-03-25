a5eeb4f72a257a798a2a6d538dad642ce6a69b9a
Jing Yu <jing.yu@cazoo.co.uk>
2021-03-25 09:46:39 +0000

    mob next [ci-skip]

--- src/main/gameOfLife.ts
+++ src/main/gameOfLife.ts
@@ -8,62 +8,62 @@ enum Status {
   Dead = 0,
 }
 
-export class GameOfLife {
-  private grid: number[][];
+class Grid {
+  private grid: Status[][];
 
-  constructor(grid: number[][]) {
+  constructor(grid: Status[][]) {
     this.grid = grid;
   }
 
-  getTopLeftNeighbourPosition({ x, y }: Coordinates): Coordinates {
-    return {
-      x: Math.max(0, x - 1),
-      y: Math.max(0, y - 1),
-    };
+  getCellStatus({ x, y }: Coordinates): Status {
+    try {
+      return this.grid[x][y];
+    } catch {
+      return Status.Dead;
+    }
   }
 
-  getBottomRightNeighbourPosition({ x, y }: Coordinates): Coordinates {
-    return {
-      x: Math.min(x + 1, this.grid.length - 1),
-      y: Math.min(y + 1, this.grid.length - 1),
-    };
-  }
-
-  isCellAlive({ x, y }: Coordinates): boolean {
-    return this.grid[x][y] === Status.Alive;
+  isCellAlive(coordinate: Coordinates): boolean {
+    return this.getCellStatus(coordinate) === Status.Alive;
   }
 
   getNumberOfAliveNeighbours({ x, y }: Coordinates): number {
+    const currentCellStatus = this.isCellAlive({ x, y }) ? 1 : 0;
     let numberOfAliveNeighbours = 0;
-
-    const topLeft = this.getTopLeftNeighbourPosition({ x, y });
-    const bottomRight = this.getBottomRightNeighbourPosition({ x, y });
-
-    for (let i = topLeft.x; i <= bottomRight.x; i++) {
-      for (let j = topLeft.y; j <= bottomRight.y; j++) {
+    for (let i = x - 1; i <= x + 1; i++) {
+      for (let j = y - 1; j <= y + 1; j++) {
         if (this.isCellAlive({ x: i, y: j })) {
           numberOfAliveNeighbours += 1;
         }
       }
     }
 
+    numberOfAliveNeighbours = numberOfAliveNeighbours - currentCellStatus;
+
     return numberOfAliveNeighbours;
   }
+}
 
-  shouldBeAlive(numberOfAliveNeighbours: number): boolean {
-    return numberOfAliveNeighbours === 3 || numberOfAliveNeighbours === 4;
+export class GameOfLife {
+  private grid: Grid;
+
+  constructor(grid: Status[][]) {
+    this.grid = new Grid(grid);
   }
 
   play(coordinates: Coordinates): Status {
-    const numberOfAliveNeighbours = this.getNumberOfAliveNeighbours(
+    const numberOfAliveNeighbours = this.grid.getNumberOfAliveNeighbours(
       coordinates
     );
 
-    if (this.isCellAlive(coordinates)) {
-      return this.shouldBeAlive(numberOfAliveNeighbours)
-        ? Status.Alive
-        : Status.Dead;
+    if (numberOfAliveNeighbours === 3) {
+      return Status.Alive;
     }
-    return numberOfAliveNeighbours === 3 ? Status.Alive : Status.Dead;
+
+    if (this.grid.isCellAlive(coordinates) && numberOfAliveNeighbours === 2) {
+      return Status.Alive;
+    }
+
+    return Status.Dead;
   }
 }
